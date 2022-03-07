f1d5fcb82043ee0df868039bacb95bc804e69862
ramin harikan <harikan2007@gmail.com>
2022-03-07 15:55:15 +0330

    with express and mongodb

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express()
 app.set("view engine","ejs")
 app.use(express.static("public"))
 app.listen(3000, ()=>{
-    console.log("app listening on port 4000")
+    console.log("app listening on port 3000")
 })
 
 /*app.get("/",(req, res)=>{
