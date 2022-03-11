38ceed5107408c27ca68c4fd673d3bbef2489e58
ramin harikan <harikan2007@gmail.com>
2022-03-11 23:48:14 +0330

    register page added

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require("express")
-const path = require("path")
 const ejs = require("ejs")
 const mongoose = require("mongoose");
 const bodyparser = require("body-parser")
-
+const fileUpload = require("express-fileupload")
+const newPostController = require("./controllers/NewPost")
+const aboutController = require("./controllers/About")
+const contactController = require("./controllers/Contact")
+const samplePageController = require("./controllers/SamplePage")
+const homeController = require("./controllers/home")
+const getPostController = require("./controllers/getPost")
+const storePostController = require("./controllers/storePost")
+const middlewarevalidation = require("./middleware/validation")
+const newUserController = require("./controllers/newUser")
+const storeUserController = require("./controllers/storeUser")
 mongoose.connect("mongodb://localhost/my_database", {useNewUrlParser:true})
 const app = express()
 app.set("view engine","ejs")
 app.use(express.static("public"))
+app.use(fileUpload())
+app.use("posts/store",middlewarevalidation)
 app.use(bodyparser.json())
 app.use(bodyparser.urlencoded({extended:true}))
 app.listen(3000, ()=>{
@@ -26,23 +37,13 @@ app.get("/contact",(req, res)=>{
 app.get("/post",(req, res)=>{
     res.sendFile(path.resolve(__dirname, "pages/post.html"))
 })*/
-app.get("/",(req, res)=>{
-    res.render("index")
-})
-app.get("/about",(req, res)=>{
-    res.render("about")
-})
-app.get("/contact",(req, res)=>{
-    res.render("contact")
-})
-app.get("/post",(req, res)=>{
-    res.render("post")
-})
-app.get("/posts/new", (req, res)=>{
-    res.render("create")
-})
-app.post("/posts/store",(req, res)=>{
-    console.log(req.body)
 
-    res.redirect("/")
-})
+app.get("/", homeController)
+app.get("/auth/register", newUserController)
+app.get("/about", aboutController)
+app.get("/contact", contactController)
+app.get("/post",samplePageController)
+app.get("/post/:id", getPostController)
+app.get("/posts/new", newPostController)
+app.post("/posts/store", storePostController)
+app.post("/users/store", storeUserController)
